228a7db5d3fea909ef72b878ad70e59cc25da452
Márcio <alvesmarcio@hotmail.com>
2022-12-11 17:56:25 -0300

    feature: create component folder

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import CalendarGrid from "./CalendarGrid";
+import Toast from "./Toast";
+import Loader from "./Loader";
+import Modal from "./Modal";
+import Event from "./Event";
+import EventForm from "./EventForm";
+import MiniEvent from "./MiniEvent";
+import DayLabels from "./DayLabel";
+import CalendarHeader from "./CalendarHeader";
+
+export {
+  CalendarGrid,
+  Toast,
+  Loader,
+  Modal,
+  Event,
+  EventForm,
+  MiniEvent,
+  DayLabels,
+  CalendarHeader,
+};
