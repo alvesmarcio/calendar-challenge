5b16d11139b4e2c8838eeaf57f41f7bf75577542
Márcio <alvesmarcio@hotmail.com>
2022-12-11 17:55:12 -0300

    feature: create generic loader component

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,14 @@
+// Loader component from https://loading.io/css
+
+const Loader = () => {
+  return (
+    <div class="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
